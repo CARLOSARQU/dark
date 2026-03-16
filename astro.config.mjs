8dcd0064917e89abafe9cb68a6a86b9e7a427664
Carlos Midwar Arque Mamani <carlosmidwar6@gmail.com>
2026-03-15 19:00:47 -0500

    se agrega funcionalidad de 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,16 @@ import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 import markdoc from '@astrojs/markdoc';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://sistemas.unap.edu.pe/', 
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [keystatic(), react(), markdoc()],
+  integrations: [keystatic(), react(), markdoc(), sitemap()],
 
-  // server: Keystatic necesita SSR; las páginas con getStaticPaths() siguen siendo estáticas
+  // server: Keystatic necesita SSR; las páginas con prerender=true siguen siendo estáticas
   output: 'server',
   adapter: node({
     mode: 'standalone',
